8f25433857df65901e9fbfdc6a124026519a4e92
Nemanja Dimitrijevic <necapro@gmail.com>
2020-02-05 00:46:58 +0100

    #trie
    Implementacija trie stabla i dodavanje reci u stablo

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import obilazakFajlova
+from analyzer import Parser
+import trie
 
-rootdir = r"C:\Users\Nemanja\PycharmProjects\PYTHON-OISISI-projekat\python-2.7.7-docs-html\extending"
+rootdir = r"C:\Users\Nemanja\PycharmProjects\PYTHON-OISISI-projekat\python-2.7.7-docs-html\faq"
 
-obilazakFajlova.obidji(rootdir)
+root = obilazakFajlova.obidji(rootdir)
 
+"""
+root = trie.TrieNode('*')
+parser = Parser()
+parser.parse(rootdir)
+i = 0
+for word in parser.words:
+    if word == 'Python':
+        i = i+1
+        print('************************** ' , i , ' ****************************')
+    else:
+        print(word)
+    trie.add(root, word)
+
+print(trie.find_prefix(root, 'Python'))
+"""
+print(trie.find_prefix(root, 'Python'))
 
--- obilazakFajlova.py
+++ obilazakFajlova.py
@@ -1,7 +1,14 @@
 import os
+from analyzer import Parser
+import trie
 
 def obidji(rootdir):
-     for subdir, dirs, files in os.walk(rootdir):
+    root = trie.TrieNode('*')
+    for subdir, dirs, files in os.walk(rootdir):
         for file in files:
             if file.endswith('.html'):
-                print(os.path.join(subdir, file))
+                parser = Parser()
+                parser.parse(os.path.join(subdir, file))
+                for word in parser.words:
+                    trie.add(root, word)
+    return root
